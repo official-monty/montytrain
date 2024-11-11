574ba1a38dce1d4e91e6a306c0c612abb7470ef1
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-11-11 03:32:07 +0000

    Update value scripts

--- value/src/arch.rs
+++ value/src/arch.rs
@@ -39,6 +39,8 @@ pub fn make_trainer(l1: usize) -> Trainer<AdamWOptimiser, ThreatInputs, outputs:
             ("l2b".to_string(), QuantTarget::Float),
             ("l3w".to_string(), QuantTarget::Float),
             ("l3b".to_string(), QuantTarget::Float),
+            ("pstw".to_string(), QuantTarget::Float),
+            ("pstb".to_string(), QuantTarget::Float),
         ],
     )
 }
@@ -48,17 +50,19 @@ fn build_network(inputs: usize, l1: usize) -> (Graph, Node) {
 
     // inputs
     let stm = builder.create_input("stm", Shape::new(inputs, 1));
-    let targets = builder.create_input("targets", Shape::new(1, 1));
+    let targets = builder.create_input("targets", Shape::new(3, 1));
 
     // trainable weights
+    let pstw = builder.create_weights("pstw", Shape::new(3, inputs));
+    let pstb = builder.create_weights("pstb", Shape::new(3, 1));
     let l0w = builder.create_weights("l0w", Shape::new(l1, inputs));
     let l0b = builder.create_weights("l0b", Shape::new(l1, 1));
     let l1w = builder.create_weights("l1w", Shape::new(16, l1));
     let l1b = builder.create_weights("l1b", Shape::new(16, 1));
     let l2w = builder.create_weights("l2w", Shape::new(128, 16));
     let l2b = builder.create_weights("l2b", Shape::new(128, 1));
-    let l3w = builder.create_weights("l3w", Shape::new(1, 128));
-    let l3b = builder.create_weights("l3b", Shape::new(1, 1));
+    let l3w = builder.create_weights("l3w", Shape::new(3, 128));
+    let l3b = builder.create_weights("l3b", Shape::new(3, 1));
 
     // inference
     let l1 = operations::affine(&mut builder, l0w, stm, l0b);
@@ -67,9 +71,12 @@ fn build_network(inputs: usize, l1: usize) -> (Graph, Node) {
     let l2 = operations::activate(&mut builder, l2, Activation::SCReLU);
     let l3 = operations::affine(&mut builder, l2w, l2, l2b);
     let l3 = operations::activate(&mut builder, l3, Activation::SCReLU);
-    let predicted = operations::affine(&mut builder, l3w, l3, l3b);
-    let sigmoided = operations::activate(&mut builder, predicted, Activation::Sigmoid);
-    operations::mse(&mut builder, sigmoided, targets);
+    let l4 = operations::affine(&mut builder, l3w, l3, l3b);
+
+    let pst = operations::affine(&mut builder, pstw, stm, pstb);
+
+    let predicted = operations::add(&mut builder, l4, pst);
+    operations::softmax_crossentropy_loss(&mut builder, predicted, targets);
 
     // graph, output node
     (builder.build(ExecutionContext::default()), predicted)
