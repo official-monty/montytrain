9115ed2b8f344c7cb4761135b8b9e99d3b3e0104
jw1912 <jamie.whiting.dev@gmail.com>
2025-01-11 13:10:56 +0000

    use threat inputs

--- value/src/inputs.rs
+++ value/src/inputs.rs
@@ -2,12 +2,21 @@ use bullet::montyformat::chess::{Attacks, Piece, Position, Side};
 
 use crate::threats::map_threat_to_index;
 
-pub const INPUT_SIZE: usize = 768;
+pub const INPUT_SIZE: usize = 768 * 4;
 pub const MAX_ACTIVE: usize = 32;
 
 pub fn map_inputs<F: FnMut(usize)>(pos: &Position, mut f: F) {
     assert_eq!(pos.stm(), Side::WHITE);
+    let flip = pos.stm() == Side::BLACK;
     let hm = if pos.king_index() % 8 > 3 { 7 } else { 0 };
+
+    let mut threats = pos.threats_by(pos.stm() ^ 1);
+    let mut defences = pos.threats_by(pos.stm());
+
+    if flip {
+        threats = threats.swap_bytes();
+        defences = defences.swap_bytes();
+    }
 
     for piece in Piece::PAWN..=Piece::KING {
         let pc = 64 * (piece - 2);
@@ -15,15 +24,44 @@ pub fn map_inputs<F: FnMut(usize)>(pos: &Position, mut f: F) {
         let mut our_bb = pos.piece(piece) & pos.piece(pos.stm());
         let mut opp_bb = pos.piece(piece) & pos.piece(pos.stm() ^ 1);
 
+        if flip {
+            our_bb = our_bb.swap_bytes();
+            opp_bb = opp_bb.swap_bytes();
+        }
+
         while our_bb > 0 {
             let sq = our_bb.trailing_zeros();
-            f(pc + (sq ^ hm) as usize);
+            let mut feat = pc + (sq ^ hm) as usize;
+
+            let bit = 1 << sq;
+            if threats & bit > 0 {
+                feat += 768;
+            }
+
+            if defences & bit > 0 {
+                feat += 768 * 2;
+            }
+
+            f(feat);
+
             our_bb &= our_bb - 1;
         }
 
         while opp_bb > 0 {
             let sq = opp_bb.trailing_zeros();
-            f(384 + pc + (sq ^ hm) as usize);
+            let mut feat = 384 + pc + (sq ^ hm) as usize;
+
+            let bit = 1 << sq;
+            if threats & bit > 0 {
+                feat += 768;
+            }
+
+            if defences & bit > 0 {
+                feat += 768 * 2;
+            }
+
+            f(feat);
+
             opp_bb &= opp_bb - 1;
         }
     }
