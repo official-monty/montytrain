f2ddb2482eba6743dbd94bdb676db78159fbc2b9
jw1912 <jamie.whiting.dev@gmail.com>
2025-02-21 01:10:41 +0000

    simplify

--- value/src/threats.rs
+++ value/src/threats.rs
@@ -10,17 +10,27 @@ pub fn map_piece_threat(
 ) -> Option<usize> {
     match piece {
         Piece::PAWN => map_pawn_threat(src, dest, enemy),
-        Piece::KNIGHT => map_knight_threat(src, dest, enemy),
-        Piece::BISHOP => map_bishop_threat(src, dest, enemy),
-        Piece::ROOK => map_rook_threat(src, dest, enemy),
-        Piece::QUEEN => map_queen_threat(src, dest, enemy),
-        Piece::KING => map_king_threat(src, dest, enemy),
+        Piece::KNIGHT => map_threat(src, dest, enemy, &indices::KNIGHT, &attacks::KNIGHT, offsets::KNIGHT),
+        Piece::BISHOP => map_threat(src, dest, enemy, &indices::BISHOP, &attacks::BISHOP, offsets::BISHOP),
+        Piece::ROOK => map_threat(src, dest, enemy, &indices::ROOK, &attacks::ROOK, offsets::ROOK),
+        Piece::QUEEN => map_threat(src, dest, enemy, &indices::QUEEN, &attacks::QUEEN, offsets::QUEEN),
+        Piece::KING => map_threat(src, dest, enemy, &indices::KING, &attacks::KING, offsets::KING),
         _ => unreachable!(),
     }
 }
 
 fn below(src: usize, dest: usize, table: &[u64; 64]) -> usize {
     (table[src] & ((1 << dest) - 1)).count_ones() as usize
+}
+
+fn map_threat(src: usize, dest: usize, enemy: bool, indices: &[usize; 65], attacks: &[u64; 64], offset: usize) -> Option<usize> {
+    let idx = indices[src] + below(src, dest, attacks);
+    let threat = offset + usize::from(enemy) * indices[64] + idx;
+
+    assert!(threat >= offset);
+    assert!(threat < offsets::END);
+
+    Some(threat)
 }
 
 fn map_pawn_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
@@ -31,56 +41,6 @@ fn map_pawn_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
     let threat = offsets::PAWN + usize::from(enemy) * indices::PAWN + (src / 8 - 1) * 14 + attack;
 
     assert!(threat < offsets::KNIGHT, "{threat}");
-
-    Some(threat)
-}
-
-fn map_knight_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
-    let idx = indices::KNIGHT[src] + below(src, dest, &attacks::KNIGHT);
-    let threat = offsets::KNIGHT + usize::from(enemy) * indices::KNIGHT[64] + idx;
-
-    assert!(threat >= offsets::KNIGHT, "{threat}");
-    assert!(threat < offsets::BISHOP, "{threat}");
-
-    Some(threat)
-}
-
-fn map_bishop_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
-    let idx = indices::BISHOP[src] + below(src, dest, &attacks::BISHOP);
-    let threat = offsets::BISHOP + usize::from(enemy) * indices::BISHOP[64] + idx;
-
-    assert!(threat >= offsets::BISHOP, "{threat}");
-    assert!(threat < offsets::ROOK, "{threat}");
-
-    Some(threat)
-}
-
-fn map_rook_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
-    let idx = indices::ROOK[src] + below(src, dest, &attacks::ROOK);
-    let threat = offsets::ROOK + usize::from(enemy) * indices::ROOK[64] + idx;
-
-    assert!(threat >= offsets::ROOK, "{threat}");
-    assert!(threat < offsets::QUEEN, "{threat}");
-
-    Some(threat)
-}
-
-fn map_queen_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
-    let idx = indices::QUEEN[src] + below(src, dest, &attacks::QUEEN);
-    let threat = offsets::QUEEN + usize::from(enemy) * indices::QUEEN[64] + idx;
-
-    assert!(threat >= offsets::QUEEN, "{threat}");
-    assert!(threat < offsets::KING, "{threat}");
-
-    Some(threat)
-}
-
-fn map_king_threat(src: usize, dest: usize, enemy: bool) -> Option<usize> {
-    let idx = indices::KING[src] + below(src, dest, &attacks::KING);
-    let threat = offsets::KING + usize::from(enemy) * indices::KING[64] + idx;
-
-    assert!(threat >= offsets::KING, "{threat}");
-    assert!(threat < offsets::END, "{threat}");
 
     Some(threat)
 }
