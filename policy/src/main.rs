72e5cdfd788f82490e1f91485750464f37a9fb4b
jw1912 <jamie.whiting.dev@gmail.com>
2025-03-05 14:40:51 +0000

    ye

--- policy/src/main.rs
+++ policy/src/main.rs
@@ -6,8 +6,7 @@ mod trainer;
 
 use bullet::{
     nn::{
-        optimiser::{AdamWParams, Optimiser},
-        Activation, ExecutionContext, Graph, NetworkBuilder, Shape,
+        optimiser::{AdamWParams, Optimiser}, Activation, ExecutionContext, Graph, NetworkBuilder, Node, Shape
     },
     trainer::{
         logger,
@@ -24,14 +23,10 @@ const ID: &str = "policy001";
 
 fn main() {
     let data_preparer = preparer::DataPreparer::new("data/policygen6.binpack", 4096);
-    let _data_preparer = preparer::DataPreparer::new(
-        "/home/privateclient/monty_value_training/interleaved.binpack",
-        96000,
-    );
 
     let size = 128;
 
-    let graph = network(size);
+    let (graph, output_node) = network(size);
 
     let optimiser_params = AdamWParams {
         decay: 0.01,
@@ -50,9 +45,9 @@ fn main() {
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
-            batches_per_superbatch: 6104,
+            batches_per_superbatch: 1024,
             start_superbatch: 1,
-            end_superbatch: 600,
+            end_superbatch: 1,
         },
         wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
         lr_scheduler: lr::ExponentialDecayLR {
@@ -91,7 +86,7 @@ fn main() {
                             SavedFormat::new(
                                 "l1w",
                                 QuantTarget::Float,
-                                Layout::Transposed(Shape::new(moves::NUM_MOVES, size / 2)),
+                                Layout::Transposed(Shape::new(moves::NUM_MOVES, 2 * size)),
                             ),
                             SavedFormat::new("l1b", QuantTarget::Float, Layout::Normal),
                         ],
@@ -100,9 +95,19 @@ fn main() {
             }
         },
     );
+
+    for fen in [
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+    ] {
+        eval_fen(&mut trainer, output_node, fen);
+    }
 }
 
-fn network(size: usize) -> Graph {
+fn network(size: usize) -> (Graph, Node) {
     let builder = NetworkBuilder::default();
 
     let stm = builder.new_sparse_input("stm", Shape::new(inputs::INPUT_SIZE, 1), inputs::MAX_ACTIVE);
@@ -117,5 +122,33 @@ fn network(size: usize) -> Graph {
     out = l1.forward(out);
     out.masked_softmax_crossentropy_loss(dist, mask);
 
-    builder.build(ExecutionContext::default())
+    let out = out.node();
+    (builder.build(ExecutionContext::default()), out)
+}
+
+fn eval_fen(trainer: &mut Trainer, output_node: Node, fen: &str) {
+    use bullet::trainer::default::formats::montyformat::chess::{Position, Castling, Move};
+
+    let mut castling = Castling::default();
+    let pos = Position::parse_fen(fen, &mut castling);
+    let mut data = loader::DecompressedData { pos, moves: [(0, 0); 108], num: 0 };
+
+    pos.map_legal_moves(&castling, |mov| {
+        data.moves[data.num] = (u16::from(mov), 1);
+        data.num += 1;
+    });
+
+    let prepared = preparer::PreparedData::new(&[data], 1);
+
+    trainer.load_batch(&prepared);
+
+    trainer.optimiser.graph.forward().unwrap();
+
+    let output = trainer.optimiser.graph.get_node(output_node).get_dense_vals().unwrap();
+
+    println!("FEN: {fen}");
+    for (mov, _) in &data.moves[..data.num] {
+        let mov = Move::from(*mov);
+        println!("{}: {}", mov.to_uci(&castling), output[moves::map_move_to_index(&pos, mov)]);
+    }
 }
